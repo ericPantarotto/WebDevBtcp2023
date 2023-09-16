be090a356bf2a3aae055ae128f1e7a7ae9ca9119
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2023-09-17 01:01:24 +0200

    Google OAuth 2.0

--- 38+Secrets_Starting_Code/db.js
+++ 38+Secrets_Starting_Code/db.js
@@ -1,7 +1,9 @@
 import "dotenv/config";
 import mongoose from "mongoose";
 import encrypt from "mongoose-encryption";
+import findOrCreate from "mongoose-findorcreate";
 import passport from "passport";
+import strategy from "passport-google-oauth20";
 import passportLocalMongoose from "passport-local-mongoose";
 
 export function connect() {
@@ -12,6 +14,8 @@ export function connect() {
 export const userSchema = new mongoose.Schema({
   email: String,
   password: String,
+  googleId: String,
+  secret: String,
 });
 userSchema.plugin(encrypt, {
   secret: process.env.SOME_LONG_UNGUESSABLE_STRING,
@@ -19,20 +23,43 @@ userSchema.plugin(encrypt, {
 });
 //NOTE: plugin to hash and salt our user's password in our mongoDB db
 userSchema.plugin(passportLocalMongoose);
+//NOTE: https://www.npmjs.com/package/mongoose-findorcreate (use for Google OAuth 2.0)
+userSchema.plugin(findOrCreate);
 
 export const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.use(
+  new strategy.Strategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "http://localhost:3001/auth/google/secrets",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
+
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
 
 passport.serializeUser(function (user, done) {
-  done(null, user.email);
+  done(null, user.id);
 });
 
-passport.deserializeUser(function (id, done) {
-  User.findOne(id, function (err, user) {
-    done(err, user);
-  });
+passport.deserializeUser(async function (id, done) {
+  await User.findOne({ _id: id })
+    .then((user) => {
+      done(null, user);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
 });

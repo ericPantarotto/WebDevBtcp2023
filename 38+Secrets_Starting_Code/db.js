c2541e3b16d904de17729e635ec1e57294716f14
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2023-09-16 19:11:54 +0200

    passport with cookies and session

--- 38+Secrets_Starting_Code/db.js
+++ 38+Secrets_Starting_Code/db.js
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import mongoose from "mongoose";
 import encrypt from "mongoose-encryption";
+import passport from "passport";
+import passportLocalMongoose from "passport-local-mongoose";
 
 export function connect() {
   mongoose.connect("mongodb://localhost:27017/userDB", {
@@ -15,5 +17,22 @@ userSchema.plugin(encrypt, {
   secret: process.env.SOME_LONG_UNGUESSABLE_STRING,
   encryptedFields: ["password"],
 });
+//NOTE: plugin to hash and salt our user's password in our mongoDB db
+userSchema.plugin(passportLocalMongoose);
 
 export const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser(function (user, done) {
+  done(null, user.email);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findOne(id, function (err, user) {
+    done(err, user);
+  });
+});

d3d005eeda1ea8c0946bd61ca2b675edb53765a1
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2023-09-28 16:08:08 +0200

    Todo Input Area component

--- todolist-app/src/components/App.jsx
+++ todolist-app/src/components/App.jsx
@@ -1,21 +1,15 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import ToDoItem from "./ToDoItem";
+import InputArea from "./InputArea";
 
 function App() {
-  const [inputText, setInputText] = useState("");
   const [items, setItems] = useState([]);
 
-  function handleChange(event) {
-    const newValue = event.target.value;
-    setInputText(newValue);
-  }
-
-  function addItem() {
+  function addItem(inputText) {
     setItems((prevItems) => {
       return [...prevItems, { id: uuidv4(), text: inputText }];
     });
-    setInputText("");
   }
   function onDelete(id) {
     console.log(id);
@@ -31,12 +25,7 @@ function App() {
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
-      <div className="form">
-        <input onChange={handleChange} type="text" value={inputText} />
-        <button onClick={addItem}>
-          <span>Add</span>
-        </button>
-      </div>
+      <InputArea onAdd={addItem} />
       <div>
         <ul>
           {items.map((todoItem, index) => (

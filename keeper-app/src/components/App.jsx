9046b7b185c3e542ae61118b302e627d0e26da2e
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2023-09-28 08:56:14 +0200

    Managing a component tree

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import notes from "../notes";
 import Footer from "./Footer";
 import Header from "./Header";
 import Note from "./Note";
 
 function App() {
+  //HACK: check chrome console: console.log(notes);
   return (
     <div>
       <Header />
-      <Note />
+      {/* <Note /> */}
+      {notes.map((noteItem) => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
       <Footer />
     </div>
   );
